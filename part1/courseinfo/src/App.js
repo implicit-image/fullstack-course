cf6d243b01a58451de22f0c98ac8387b42dacbce
Błażej Niewiadomski <blaz.nie@protonmail.com>
2021-07-08 23:53:09 +0200

    finished exercise 1.4

--- part1/courseinfo/src/App.js
+++ part1/courseinfo/src/App.js
@@ -30,10 +30,10 @@ const Part = ({name, exercises}) => {
   )
 }
 
-const Total = ({exercises}) => {
+const Total = ({parts}) => {
   return (
     <>
-      <p>Number of exercises {exercises.reduce((a, b) => {return a + b})}</p>
+      <p>Number of exercises {parts.map(p => p.exercises ).reduce((a, b) => a + b )}</p>
     </>
   )
 }
@@ -41,31 +41,29 @@ const Total = ({exercises}) => {
 
 const App = () => {
   const course = "Half Stack application developement"
-  const part1 = {
-    name: "Fundamentals of React",
-    exercises: 10
-  }
-  const part2 = {
-    name: "Using props to pass data",
-    exercises: 7
-  }
-  const part3 = {
-    name: "State of a component",
-    exercises: 14
-  }
+  const parts = [
+    {
+      name: "Fundamentals of React",
+      exercises: 10
+    },
+    {
+      name: "Using props to pass data",
+      exercises: 7
+    },
+    {
+      name: "State of a component",
+      exercises: 14
+    }]
 
 
-  const parts = [part1, part2, part3]
   const exercises = parts.map(p => p.exercises)
 
 
   return (
     <div>
-      <Header name={course}/>
-      <Content
-        parts={parts}
-      />
-      <Total exercises={exercises}/>
+      <Header name={course} />
+      <Content parts={parts} />
+      <Total parts={parts} />
     </div>
   );
 }
