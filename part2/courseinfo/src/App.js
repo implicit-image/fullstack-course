57c40aa8877746fe0e4253d284fca86d279edab7
Błażej Niewiadomski <blaz.nie@protonmail.com>
2021-07-15 16:27:49 +0200

    finished exercise 2.1

---
+++ part2/courseinfo/src/App.js
@@ -0,0 +1,81 @@
+import React from 'react';
+//import ReactDOM from 'react-dom';
+
+const Header = ({ course }) => {
+  return (
+    <h1>{course.name}</h1>
+  )
+}
+
+const Total = ({ course }) => {
+  const sum = course.parts
+                    .map(p => p.exercises)
+                    .reduce((e, f) => { return e + f})
+  return(
+    <p>Number of exercises {sum}</p>
+  )
+}
+
+const Part = ({ part }) => {
+  return (
+    <p>
+      {part.name} {part.exercises}
+    </p>
+  )
+}
+
+const Content = ({ course }) => {
+  return (
+    <>
+      {course.parts.map(p => {
+        return (
+          <div key={p.id}>
+            <Part part={p}/>
+          </div>
+        )
+      })}
+    </>
+  )
+}
+
+const Course = ({course}) => {
+  return (
+    <>
+      <Header course={course}/>
+      <Content course={course}/>
+      <Total course={course}/>
+    </>
+  )
+}
+
+const App = () => {
+  const course = {
+    id: 1,
+    name: 'Half Stack application development',
+    parts: [
+      {
+        id: 1,
+        name: 'Fundamentals of React',
+        exercises: 10,
+      },
+      {
+        id: 2,
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        id: 3,
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Course course={course}/>
+    </div>
+  )
+}
+
+export default App;
