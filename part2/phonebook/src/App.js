0682ec252da79626ada01cd2406c9cf90c34103e
Błażej Niewiadomski <blaz.nie@protonmail.com>
2021-07-18 22:20:53 +0200

    finished exercise 2.8

--- part2/phonebook/src/App.js
+++ part2/phonebook/src/App.js
@@ -7,7 +7,7 @@ import './App.css'
 const Numbers = ({ persons }) => {
   return (
     <div>
-      {persons.map(person => <li key={person.name}>{person.name}</li>)}
+    {persons.map(person => <li key={person.number}>{person.name} {person.number}</li>)}
     </div>
   )
 }
@@ -16,10 +16,14 @@ const Numbers = ({ persons }) => {
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: "Arto Hellas" }
+    { name: "Arto Hellas", number: "111111111" }
   ])
-
   const [newName, setNewName] = useState("")
+  const [newNumber, setNewNumber] = useState("")
+
+  const handleNumberChange = (event) => {
+    setNewNumber(event.target.value)
+  }
 
 //run every time letter is added or removed from text field
   const handleNameChange = (event) => {
@@ -30,18 +34,19 @@ const App = () => {
     event.preventDefault()
 
     const newPerson = {
-      name: newName
+      name: newName,
+      number: newNumber
     }
 
 
-    if ((!persons.some(person => person.name === newPerson.name ))) {
+    if ((!persons.some(person => person.number === newPerson.number ))) {
       setPersons(persons.concat(newPerson))
       setNewName("")
+      setNewNumber("")
     }
     else {
-      alert(`${newName} is already in the phonebook`)
+      alert(`${newNumber} is already in the phonebook`)
     }
-
   }
 
   return (
@@ -53,6 +58,13 @@ const App = () => {
           <input
             value={newName} // so we can reset field content
             onChange={handleNameChange}
+          />
+        </div>
+        <div>
+          number:
+          <input
+            value={newNumber}
+            onChange={handleNumberChange}
           />
         </div>
         <div>
